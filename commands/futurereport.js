a4ab61d90b5e7abf43d303f4748c7b94111b1f33
Griffin Freiberg <freiberg.griffin@gmail.com>
2020-11-21 15:49:43 -0500

    updating

--- commands/futurereport.js
+++ commands/futurereport.js
@@ -1,9 +1,31 @@
 /* eslint-disable no-unused-vars */
+const Discord = require('discord.js');
+const si = require('stock-info');
 
 module.exports = {
-	name: 'ping',
-	description: 'Ping!',
+	name: 'futurereport',
+	description: 'Fetches information about various futures then sends them in an embed',
 	execute(message, args) {
-		message.channel.send('Pong.');
+        const futurereport = (stocks) => {
+            const report = new Discord.MessageEmbed()
+                .setColor("gold")
+                .setTitle("Future Report")
+                .setAuthor("Vision Futures Bot")
+                .setDescription("The following prices were retrieved from the Yahoo Finance API")
+                .addFields(
+                    { name: 'YM=F - Dow Jones', value: stocks[0].regularMarketPrice + " " + stocks[0].regularMarketChange + "(" + stocks[0].regularMarketChangePercent + "%)"},
+                    { name: 'NQ=F - NASDAQ', value: stocks[1].regularMarketPrice + " " + stocks[1].regularMarketChange + "(" + stocks[1].regularMarketChangePercent + "%)"},
+                    { name: 'ES=F - S&P 500', value: stocks[2].regularMarketPrice + " " + stocks[2].regularMarketChange + "(" + stocks[2].regularMarketChangePercent + "%)"},
+                    { name: 'CL=F - Crude Oil', value: stocks[3].regularMarketPrice + " " + stocks[3].regularMarketChange + "(" + stocks[3].regularMarketChangePercent + "%)"},
+                    { name: 'GC=F - Gold', value: stocks[4].regularMarketPrice + " " + stocks[4].regularMarketChange + "(" + stocks[4].regularMarketChangePercent + "%)"},
+                )
+                .setFooter("Copyright © 2020 Trade With Vision")
+                .setTimestamp()
+                message.channel.send(report);
+        }
+
+        si.getStocksInfo(["YM=F","NQ=F","ES=F","CL=F","GC=F"]).then(
+            futurereport
+        )
 	},
 };
--- commands/getstock.js
+++ commands/getstock.js
@@ -1,9 +1,18 @@
 /* eslint-disable no-unused-vars */
+const si = require('stock-info');
 
 module.exports = {
 	name: 'getstock',
 	description: 'Fetches a stock by the ticker and sends a message with the price of the stocks',
 	execute(message, args) {
-		message.channel.send('Pong.');
+        const ticker = args[0].toUpperCase();
+        const getstock = (stock) => {
+            const price = stock.regularMarketPrice;
+            message.channel.send(`The current market price of ${ticker} is: ${price}`);
+        }
+        
+        si.getSingleStockInfo(args[0]).then(
+            getstock
+        )
 	},
 };
